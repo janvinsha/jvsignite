e4f3218cf526b2218ce2d83403dbe9058c8f378e
Janvinsha <janvinsha@gmail.com>
2021-09-12 07:26:52 +0100

    update

--- src/api.js
+++ src/api.js
@@ -27,20 +27,20 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10&key=7397d43b67c441d8a8e424642cd086c5`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10&key=7397d43b67c441d8a8e424642cd086c5`;
+const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10&key=7397d43b67c441d8a8e424642cd086c5`;
 
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
 export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
 export const newGamesUrl = () => `${base_url}${new_games}`;
 
 //GAME DETAIL
-export const gamesDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gamesDetailsURL = (game_id) => `${base_url}games/${game_id}?key=7397d43b67c441d8a8e424642cd086c5`;
 //GAME SCREENSHOTS
 export const gamesScreenshotsURL = (game_id) => {
-  return `${base_url}games/${game_id}/screenshots`;
+  return `${base_url}games/${game_id}/screenshots?key=7397d43b67c441d8a8e424642cd086c5`;
 };
 //Searched game
 export const searchGameURL = (game_name) =>
-  `${base_url}games?search=${game_name}&page_size=9`;
+  `${base_url}games?search=${game_name}&page_size=9&key=7397d43b67c441d8a8e424642cd086c5`;
